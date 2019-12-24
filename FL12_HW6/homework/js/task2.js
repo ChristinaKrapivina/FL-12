13c916db38990aa2190302a3e9410cbf36d0f60b
Christina Krapivina <kalista5015@gmail.com>
2019-12-24 17:39:34 +0200

    Update task2.js - final

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,23 +1,19 @@
-let a = Number(prompt('Please enter the length of the first side of the triangle:'));
-let b = Number(prompt('Please enter the length of the second side of the triangle:'));
-let c = Number(prompt('Please enter the length of the third side of the triangle:'));
+let a = parseInt(prompt('Please enter the length of the first side of the triangle:'), 10);
+let b = parseInt(prompt('Please enter the length of the second side of the triangle:'), 10);
+let c = parseInt(prompt('Please enter the length of the third side of the triangle:'), 10);
 
-if (
-    a === 0 || isNaN(a) || 
-    b === 0 || isNaN(b) ||
-    c === 0 || isNaN(c)
-    ) {
+if (isNaN(a) || isNaN(b) || isNaN(c)) {
     alert('Input values should be ONLY numbers');
 } else if (a === 0 || b === 0 || c === 0) {
     alert('A triangle must have 3 sides with a positive definite length');
 } else if (a + b < c || b + c < a || c + a < b) {
     alert('Triangle doesn\'t exist');
 } else {
-    if (a === b && b === c && a === c) {
+    if (a === b && b === c) {
         console.log('Equilateral triangle');
-    } else if (a === b && a !== c || b === c && b !== a || a === c && a !== b) {
+    } else if (a === b || b === c || a === c) {
         console.log('Isosceles triangle');
-    } else if (a !== b && b !== c && a !== c) {
+    } else {
         console.log('Scalene triangle');
     }
 }
