4c2e9efd300882f3cd76c101c32f0fde2036d44c
Christina Krapivina <kalista5015@gmail.com>
2019-12-24 14:54:21 +0200

    Adding js - version 2

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,24 +1,24 @@
-var a = Number(prompt("Please enter value 'a' for quadratic equation:"));
-var b = Number(prompt("Please enter value 'b' for quadratic equation:"));
-var c = Number(prompt("Please enter value 'c' for quadratic equation:"));
+let a = Number(prompt('Please enter value \'a\' for quadratic equation:'));
+let b = Number(prompt('Please enter value \'b\' for quadratic equation:'));
+let c = Number(prompt('Please enter value \'c\' for quadratic equation:'));
 
-if (isNaN(a) | a == 0 | isNaN(b) | isNaN(c)) {
-    console.log("Invalid input data");
-}
-else {
-    var discrim = (b * b) - (4 * a * c);
+if (isNaN(a) || a === 0 || isNaN(b) || isNaN(c)) {
+    console.log('Invalid input data'); 
+} else {
+    const four = 4;
+    const two = 2;
+    let discrim = b * b - four * a * c;
+    let denominator = two * a;
 
     if (discrim < 0) {
-        console.log("There are no solutions to this problem");
-    }
-    else if (discrim == 0) {
-        x = - (b / (2* a));
-        console.log("x1 = x2 = " + x + ".");
-    }
-    else if (discrim > 0) {
-        x1 = (-b + Math.sqrt(discrim)) / (2 * a);
-        x2 = (-b - Math.sqrt(discrim)) / (2 * a);
-        console.log("x1 = " + x1 + " and x2 = " + x2 + ".");
-    }
+        console.log('There are no solutions to this problem'); 
+    } else if (discrim === 0) {
+        let x = - (b / denominator);
+        console.log('x1 = x2 = ' + x + '.'); 
+    } else if (discrim > 0) {
+        let x1 = (-b + Math.sqrt(discrim)) / denominator;
+        let x2 = (-b - Math.sqrt(discrim)) / denominator;
+        console.log('x1 = ' + x1 + ' and x2 = ' + x2 + '.'); 
+    } 
 }
 
